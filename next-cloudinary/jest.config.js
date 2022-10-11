163b015339bee1cebf7c4b3c4d7a718c31396822
colbyfayock <fayock@gmail.com>
2022-10-11 13:59:41 -0400

    setting up loader to allow external configuration for testing purposes, adding loader tests

--- next-cloudinary/jest.config.js
+++ next-cloudinary/jest.config.js
@@ -1,8 +1,3 @@
-/*
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
-
 module.exports = {
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
