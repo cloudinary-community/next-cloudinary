f7eeeb7a2519c6858f3c27c4fd735b33be8c86a1
codingis4noobs2 <87560178+codingis4noobs2@users.noreply.github.com>
2022-10-16 19:42:15 +0530

    Update cropping.js
    Fix "gravity" is read-only

--- next-cloudinary/src/plugins/cropping.js
+++ next-cloudinary/src/plugins/cropping.js
@@ -3,26 +3,26 @@ const cropsGravityAuto = [ 'crop', 'fill', 'lfill', 'fill_pad', 'thumb' ];
 export const props = ['crop', 'gravity'];
 
 export function plugin({ cldImage, options, cldOptions } = {}) {
-  const { width, height } = options;
-  const { crop = 'limit', gravity } = cldOptions;
-
-  let transformationString = `c_${crop},w_${width}`;
-
-  if ( !gravity && cropsGravityAuto.includes(crop) ) {
-    gravity = 'auto';
-  }
-
-  if ( !['limit'].includes(crop) ) {
-    transformationString = `${transformationString},h_${height}`;
-  }
-
-  if ( gravity ) {
-    if ( gravity === 'auto' && !cropsGravityAuto.includes(crop) ) {
-      console.warn('Auto gravity can only be used with crop, fill, lfill, fill_pad or thumb. Not applying gravity.');
-    } else {
-      transformationString = `${transformationString},g_${gravity}`;
+    const { width, height } = options;
+    const { crop = 'limit', gravity } = cldOptions;
+    
+    let transformationString = `c_${crop},w_${width}`;
+    
+    if ( !gravity && cropsGravityAuto.includes(crop) ) {
+        cldOptions.gravity = 'auto';
     }
-  }
+    
+    if ( !['limit'].includes(crop) ) {
+        transformationString = `${transformationString},h_${height}`;
+    }
+    
+    if ( cldOptions.gravity ) {
+        if ( cldOptions.gravity === 'auto' && !cropsGravityAuto.includes(crop) ) {
+        console.warn('Auto gravity can only be used with crop, fill, lfill, fill_pad or thumb. Not applying gravity.');
+        } else {
+        transformationString = `${transformationString},g_${cldOptions.gravity}`;
+        }
+    }
 
-  cldImage.resize(transformationString);
+    cldImage.resize(transformationString);
 }
