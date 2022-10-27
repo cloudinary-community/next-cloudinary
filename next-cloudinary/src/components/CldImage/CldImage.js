fe0d75e1bb26bdc6965ced33f968537dda035e95
colbyfayock <fayock@gmail.com>
2022-10-27 15:44:19 -0400

    package upgrades

--- next-cloudinary/src/components/CldImage/CldImage.js
+++ next-cloudinary/src/components/CldImage/CldImage.js
@@ -1,4 +1,4 @@
-import Image from 'next/future/image';
+import Image from 'next/image';
 
 import { createPlaceholderUrl, getPublicId } from '../../lib/cloudinary';
 import { cloudinaryLoader, transformationPlugins } from '../../loaders/cloudinary-loader';
