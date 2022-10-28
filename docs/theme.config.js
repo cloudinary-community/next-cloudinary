3ddb30ba0640d3bcf0a200e87247e6c955734091
colbyfayock <fayock@gmail.com>
2022-10-28 02:06:23 -0400

    meta

--- docs/theme.config.js
+++ docs/theme.config.js
@@ -25,22 +25,6 @@ export default {
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       <link rel="manifest" href="/site.webmanifest" />
       <meta name="theme-color" content="#000000" />
-
-      <CldOgImage
-        src={`${process.env.EXAMPLES_DIRECTORY}/turtle`}
-        tint="100:0000FF:0p:FF1493:100p"
-        blur="2000"
-        overlays={[{
-          text: {
-            color: 'white',
-            fontFamily: 'Source Sans Pro',
-            fontSize: 200,
-            fontWeight: 'bold',
-            text: 'Next Cloudinary'
-          }
-        }]}
-        twitterTitle="Next Cloudinary"
-      />
     </>
   ),
   prevLinks: false,
