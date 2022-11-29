aace374927be74fe91d853fa0c3491e28fd4790b
Colby Fayock <colbyfayock@users.noreply.github.com>
2022-11-29 12:57:32 -0500

    feat: Zoom (#71)
    # Description
    
    Adds the ability to use the zoom prop when using crop modes crop and
    thumb.
    
    This additionally fixes / corrects an issue where the responsive
    generation would add the sizes to the image URLs, upscaling the images
    unnecessarily, which adds more page load unnecessarily and creates
    inconsistencies with overlays with different sizes. The result is that
    the width/height must or should be set to the image's actual
    width/height to allow the generation to properly resize.
    
    ## Issue Ticket Number
    
    Fixes #70
    
    <!-- Specify above which issue this fixes by referencing the issue
    number (`#<ISSUE_NUMBER>`) or issue URL. -->
    <!-- Example: Fixes
    https://github.com/colbyfayock/next-cloudinary/issues/<ISSUE_NUMBER> -->
    
    ## Type of change
    
    <!-- Please select all options that are applicable. -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Fix or improve the documentation
    - [ ] This change requires a documentation update
    
    
    # Checklist
    
    <!-- These must all be followed and checked. -->
    
    - [ ] I have followed the contributing guidelines of this project as
    mentioned in [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [ ] I have created an
    [issue](https://github.com/colbyfayock/next-cloudinary/issues) ticket
    for this PR
    - [ ] I have checked to ensure there aren't other open [Pull
    Requests](https://github.com/colbyfayock/next-cloudinary/pulls) for the
    same update/change?
    - [ ] I have performed a self-review of my own code
    - [ ] I have run tests locally to ensure they all pass
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes needed to the documentation

--- next-cloudinary/src/loaders/cloudinary-loader.js
+++ next-cloudinary/src/loaders/cloudinary-loader.js
@@ -6,6 +6,9 @@ export function cloudinaryLoader({ loaderOptions, imageProps, cldOptions, cldCon
     ...cldOptions
   }
 
+  options.width = parseInt(options.width);
+  options.height = parseInt(options.height);
+
   // The loader options are used to create dynamic sizing when working with responsive images
   // so these should override the default options collected from the props alone if
   // the results are different. While we don't always use the height in the loader logic,
@@ -13,8 +16,8 @@ export function cloudinaryLoader({ loaderOptions, imageProps, cldOptions, cldCon
 
   if ( loaderOptions?.width && loaderOptions.width !== options.width ) {
     const multiplier = loaderOptions.width / options.width;
-    options.width = loaderOptions.width;
-    options.height = Math.round(options.height * multiplier);
+    options.widthResize = loaderOptions.width;
+    options.heightResize = Math.round(options.height * multiplier);
   }
 
   return constructCloudinaryUrl({
--- next-cloudinary/src/plugins/cropping.js
+++ next-cloudinary/src/plugins/cropping.js
@@ -1,9 +1,16 @@
 const cropsGravityAuto = [ 'crop', 'fill', 'lfill', 'fill_pad', 'thumb' ];
+const cropsWithZoom = ['crop', 'thumb'];
 
-export const props = ['crop', 'gravity'];
+export const props = [
+  'crop',
+  'gravity',
+  'zoom'
+];
 
 export function plugin({ cldImage, options } = {}) {
-  const { width, height, crop = 'limit' } = options;
+  const overrides = {};
+
+  const { width, height, widthResize, heightResize, crop = 'limit' } = options;
 
   let transformationString = `c_${crop},w_${width}`;
 
@@ -17,11 +24,31 @@ export function plugin({ cldImage, options } = {}) {
 
   if ( options.gravity ) {
     if ( options.gravity === 'auto' && !cropsGravityAuto.includes(crop) ) {
-      console.warn('Auto gravity can only be used with crop, fill, lfill, fill_pad or thumb. Not applying gravity.');
+      console.warn(`Auto gravity can only be used with crop modes: ${cropsGravityAuto.join(', ')}. Not applying gravity.`);
     } else {
       transformationString = `${transformationString},g_${options.gravity}`;
     }
   }
 
-  cldImage.resize(transformationString);
+  if ( options.zoom ) {
+    if ( options.zoom === 'auto' && !cropsWithZoom.includes(crop) ) {
+      console.warn(`Zoom can only be used with crop modes: ${cropsWithZoom.join(', ')}. Not applying zoom.`);
+    } else {
+      transformationString = `${transformationString},z_${options.zoom}`;
+    }
+  }
+
+  cldImage.effect(transformationString);
+
+  // If we have a resize width that's smaller than the user-defined width, we want to give the
+  // ability to perform a final resize on the image without impacting any of the effects like text
+  // overlays that may depend on the size to work properly
+
+  if ( typeof widthResize === 'number' && widthResize < width ) {
+    overrides.width = widthResize;
+  }
+
+  return {
+    options: overrides
+  }
 }
