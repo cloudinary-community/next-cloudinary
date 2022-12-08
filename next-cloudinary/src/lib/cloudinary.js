12fbc0ada8eab5136e724a3b448b12dda39e61e8
Colby Fayock <colbyfayock@users.noreply.github.com>
2022-12-08 13:07:02 -0500

    Update cloudinary.js

--- next-cloudinary/src/lib/cloudinary.js
+++ next-cloudinary/src/lib/cloudinary.js
@@ -3,6 +3,7 @@ import { Cloudinary } from '@cloudinary/url-gen';
 import * as croppingPlugin from '../plugins/cropping';
 import * as effectsPlugin from '../plugins/effects';
 import * as overlaysPlugin from '../plugins/overlays';
+import * as namedTransformationsPlugin from '../plugins/named-transformations';
 import * as rawTransformationsPlugin from '../plugins/raw-transformations';
 import * as removeBackgroundPlugin from '../plugins/remove-background';
 import * as underlaysPlugin from '../plugins/underlays';
@@ -21,6 +22,7 @@ export const transformationPlugins = [
   croppingPlugin,
   effectsPlugin,
   overlaysPlugin,
+  namedTransformationsPlugin,
   underlaysPlugin,
   zoompanPlugin,
 
@@ -63,7 +65,18 @@ export function constructCloudinaryUrl({ options, config }) {
     if ( pluginOptions?.format ) {
       options.format = pluginOptions.format;
     }
+
+    if ( pluginOptions?.width ) {
+      options.resize = {
+        width: pluginOptions?.width
+      };
+    }
   });
+
+  if ( options.resize ) {
+    const { width, crop = 'scale' } = options.resize;
+    cldImage.effect(`c_${crop},w_${width}`);
+  }
 
   return cldImage
           .setDeliveryType(options.deliveryType || 'upload')
@@ -106,6 +119,37 @@ export function getPublicId(src) {
 }
 
 /**
+ * Retrieves the transformations added to a cloudiary image url. If no transformation is recognized it returns an empty array.
+ *
+ * @param {string} src The cloudiary url.
+ *
+ * @return {array} The array of transformations
+ */
+
+ export function getTransformations(src, preserveTransformations) {
+  if (typeof src !== "string") {
+    throw new Error(`Invalid src of type ${typeof src}`);
+  }
+
+  if (src.includes("res.cloudinary.com") && preserveTransformations) {
+    const regex = new RegExp(
+      "(https?)://(res.cloudinary.com)/([^/]+)/(image|video|raw)/(upload|authenticated)/(.*)/(v[0-9]+)/(.+)(?:.[a-z]{3})?",
+      "gi"
+    );
+    const groups = regex.exec(src);
+    const transformationStr = groups.slice(1).find((i) => i.includes("_"));
+
+    if (transformationStr) {
+      return transformationStr.split(",").join("/").split("/");
+    } else {
+      return [];
+    }
+  }
+
+  return [];
+}
+
+/**
  * createPlaceholderUrl
  */
 
@@ -131,4 +175,29 @@ export function createPlaceholderUrl({ src, placeholder = true, config }) {
     },
     config
   });;
+}
+
+/**
+ * pollForProcessingImage
+ */
+
+export async function pollForProcessingImage(options) {
+  const { src } = options;
+  try {
+    await new Promise((resolve, reject) => {
+      fetch(src).then(res => {
+        if ( !res.ok ) {
+          reject(res);
+          return;
+        }
+        resolve(res);
+      });
+    });
+  } catch(e) {
+    if ( e.status === 423 ) {
+      return await pollForProcessingImage(options);
+    }
+    return false;
+  }
+  return true;
 }
