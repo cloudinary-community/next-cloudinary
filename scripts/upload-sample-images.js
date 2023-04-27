1b6f2803073d9be9b9d2680186c5605c3f89118f
Colby Fayock <fayock@gmail.com>
2023-04-27 13:18:46 -0400

    updating contributing script and adding additional assets

--- scripts/upload-sample-images.js
+++ scripts/upload-sample-images.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 
+const assets = require('./assets.json');
 const images = require('./images.json');
+const videos = require('./videos.json');
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -9,24 +11,45 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-const folder = process.env.CLOUDINARY_IMAGES_DIRECTORY || 'images';
-
 (async function run() {
-  console.log(`Uploading ${images.length} images to cloud "${process.env.CLOUDINARY_CLOUD_NAME}"...`);
+  
+  await uploadAssets(assets, {
+    folder: process.env.CLOUDINARY_ASSETS_DIRECTORY || 'assets',
+    resourceType: 'image'
+  });
+  
+  await uploadAssets(images, {
+    folder: process.env.CLOUDINARY_IMAGES_DIRECTORY || 'images',
+    resourceType: 'image'
+  });
 
-  for ( let i = 0; i < images.length; i++ ) {
-    const { url, public_id } = images[i];
+  await uploadAssets(videos, {
+    folder: process.env.CLOUDINARY_VIDEOS_DIRECTORY || 'videos',
+    resourceType: 'video'
+  });
+
+  console.log('Finished.');
+})();
+
+/**
+ * uploadAssets
+ */
+
+async function uploadAssets(assets, { folder, resourceType }) {
+  console.log(`Uploading ${assets.length} assets to cloud "${process.env.CLOUDINARY_CLOUD_NAME}" in folder ${folder}...`);
+
+  for ( let i = 0; i < assets.length; i++ ) {
+    const { url, publicId, resourceType: assetResourceType } = assets[i];
 
     try {
       const result = await cloudinary.uploader.upload(url, {
         folder,
-        public_id
+        public_id: publicId,
+        resource_type: assetResourceType || resourceType
       });
       console.log(`Success: ${result.secure_url}`);
     } catch(e) {
-      console.log(`Error uploading ${public_id}: ${e.message}`);
+      console.log(`Error uploading ${publicId}: ${e.message}`);
     }
   }
-
-  console.log('Finished.');
-})();
+}
