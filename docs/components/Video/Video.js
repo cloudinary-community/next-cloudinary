00d99f2577d2fc64d8f2ff9f50d5c44eb05d1dc9
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2023-10-14 00:04:46 +0530

    fix-lazy-load

--- docs/components/Video/Video.js
+++ docs/components/Video/Video.js
@@ -1,4 +1,5 @@
 import { getUrlParamsFromString } from '../../lib/parse';
+import { useEffect, useRef, useState } from 'react';
 
 import styles from './Video.module.scss';
 
@@ -10,26 +11,68 @@ export const Video = ({
   height = 315,
 }) => {
   const videoId = getUrlParamsFromString(url).find(({ key }) => key === 'v')?.value;
+  const [isVideoVisible, setIsVideoVisible] = useState(false);
+  const videoContainerRef = useRef();
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5, // Adjust this threshold as needed
+    };
+
+    let observer;
+    
+    if (videoContainerRef.current) {
+      observer = new IntersectionObserver(handleIntersection, options);
+      observer.observe(videoContainerRef.current);
+    }
+
+    return () => {
+      if (observer && videoContainerRef.current) {
+        observer.unobserve(videoContainerRef.current);
+      }
+    };
+  }, []);
+
+  const handleIntersection = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        setIsVideoVisible(true);
+      }
+    });
+  };
 
   return (
     <figure className={styles.video}>
-      <div
-        className={styles.videoContainer}
-        style={{
-          paddingTop: `${(height / width) * 100}%`,
-        }}
-      >
-        <iframe
-          title={title}
-          width={width}
-          height={height}
-          src={`https://www.youtube.com/embed/${videoId}?feature=oembed`}
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-          autoPlay
-        />
-      </div>
+      {isVideoVisible ? (
+        <div
+          ref={videoContainerRef}
+          className={styles.videoContainer}
+          style={{
+            paddingTop: `${(height / width) * 100}%`,
+          }}
+        >
+          <iframe
+            title={title}
+            width={width}
+            height={height}
+            src={`https://www.youtube.com/embed/${videoId}?feature=oembed`}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            autoPlay
+          />
+        </div>
+      ) : (
+        <div
+          ref={videoContainerRef}
+          style={{
+            width: '100%',
+            height: '100%',
+          }}
+        ></div>
+      )}
       <figcaption>
         <a href={url} rel="noopener" target="_blank">
           View on YouTube
