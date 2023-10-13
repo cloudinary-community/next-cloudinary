482f3f4380ac98a9d345b5fd4bd896d9370cafe8
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2023-10-13 09:32:42 +0530

    deleted:    .github/workflows/analyze.yml 	modified:   docs/next.config.js 	modified:   docs/package.json

--- docs/next.config.js
+++ docs/next.config.js
@@ -3,14 +3,6 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.js'
 });
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-
-module.exports = withBundleAnalyzer({
-  reactStrictMode: true,
-})
-
 module.exports = withNextra({
   env: {
     ASSETS_DIRECTORY: process.env.NEXT_PUBLIC_CLOUDINARY_ASSETS_DIRECTORY || 'assets',
