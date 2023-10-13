1cc4865790d869cb4c747b2d9b56bed2668572d2
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2023-10-13 09:30:12 +0530

    Revert "Create analyze.yml"
    This reverts commit 8d5a3c35ec77f5cea52ba8afe8690b77ff615f3e.

--- docs/next.config.js
+++ docs/next.config.js
@@ -3,6 +3,14 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.js'
 });
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({
+  reactStrictMode: true,
+})
+
 module.exports = withNextra({
   env: {
     ASSETS_DIRECTORY: process.env.NEXT_PUBLIC_CLOUDINARY_ASSETS_DIRECTORY || 'assets',
