6f7aada421c2b0e365259f38d48d0d540376e68d
Colby Fayock <colbyfayock@users.noreply.github.com>
2023-11-09 15:18:48 -0500

    docs: CldImage Docs Page Rework (#339)
    # Description
    
    <!-- Include a summary of the change made and also list the dependencies
    that are required if any -->
    
    ## Issue Ticket Number
    
    Fixes #<ISSUE_NUMBER>
    
    <!-- Specify above which issue this fixes by referencing the issue
    number (`#<ISSUE_NUMBER>`) or issue URL. -->
    <!-- Example: Fixes
    https://github.com/colbyfayock/next-cloudinary/issues/<ISSUE_NUMBER> -->
    
    ## Type of change
    
    <!-- Please select all options that are applicable. -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Fix or improve the documentation
    - [ ] This change requires a documentation update
    
    
    # Checklist
    
    <!-- These must all be followed and checked. -->
    
    - [ ] I have followed the contributing guidelines of this project as
    mentioned in [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [ ] I have created an
    [issue](https://github.com/colbyfayock/next-cloudinary/issues) ticket
    for this PR
    - [ ] I have checked to ensure there aren't other open [Pull
    Requests](https://github.com/colbyfayock/next-cloudinary/pulls) for the
    same update/change?
    - [ ] I have performed a self-review of my own code
    - [ ] I have run tests locally to ensure they all pass
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes needed to the documentation

---
+++ docs/components/Table/Table.js
@@ -0,0 +1,51 @@
+const Table = ({ columns, data }) => {
+  return (
+    <table className="block overflow-x-scroll scrollbar w-full text-sm text-left text-gray-500 dark:text-gray-400 mt-6 p-0 first:mt-0">
+      {Array.isArray(columns) && (
+        <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
+          <tr className="border-b border-gray-300">
+            {columns.map(({ title, id }) => {
+              return (
+                <th key={id} scope="col" className="px-6 py-4 font-bold first:pl-0 last pr-0">
+                  { title }
+                </th>
+              )
+            })}
+          </tr>
+        </thead>
+      )}
+      {Array.isArray(data) && (
+        <tbody>
+          {data.map((row, index) => {
+            return (
+              <tr key={index} className="border-b last:border-none bg-white border-gray-200 dark:bg-gray-800 dark:border-gray-800">
+                { columns.map(({ id }, index) => {
+                  let Child = row[id];
+
+                  if ( typeof Child === 'function' ) {
+                    Child = <Child />
+                  }
+
+                  if ( index === 0  ) {
+                    return (
+                      <th key={`${index}-${id}`} scope="row" className="px-6 py-3 font-normal text-gray-900 whitespace-nowrap dark:text-white first:pl-0">
+                        { Child }
+                      </th>
+                    )
+                  }
+                  return (
+                    <td key={`${index}-${id}`} className="px-6 py-3 last:pr-0">
+                      { Child }
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      )}
+    </table>
+  )
+}
+
+export default Table;
