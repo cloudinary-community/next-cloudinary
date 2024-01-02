5b4a158832a26f5a0df0ee21ba8cd016b3f20475
Colby Fayock <colbyfayock@users.noreply.github.com>
2024-01-02 12:44:23 -0500

    feat: Reverts Subpath Imports (#393)
    # Description
    
    https://github.com/colbyfayock/next-cloudinary/pull/371 introduced
    subpath imports, undocumented, as a way to import helpers without the
    baggage of React and other dependencies.
    
    This ended up having implications when dealing with importing Next.js
    components into this package's components, and was unable to find a good
    solution that fixed all scenarios that resolved all issues in both the
    Pages and App router.
    
    Subpath modules will be re-added via #363 as a breaking change to
    resolve any compatibility issues.
    
    ## Issue Ticket Number
    
    Fixes #387 
    
    <!-- Specify above which issue this fixes by referencing the issue
    number (`#<ISSUE_NUMBER>`) or issue URL. -->
    <!-- Example: Fixes
    https://github.com/colbyfayock/next-cloudinary/issues/<ISSUE_NUMBER> -->
    
    ## Type of change
    
    <!-- Please select all options that are applicable. -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Fix or improve the documentation
    - [ ] This change requires a documentation update
    
    
    # Checklist
    
    <!-- These must all be followed and checked. -->
    
    - [ ] I have followed the contributing guidelines of this project as
    mentioned in [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [ ] I have created an
    [issue](https://github.com/colbyfayock/next-cloudinary/issues) ticket
    for this PR
    - [ ] I have checked to ensure there aren't other open [Pull
    Requests](https://github.com/colbyfayock/next-cloudinary/pulls) for the
    same update/change?
    - [ ] I have performed a self-review of my own code
    - [ ] I have run tests locally to ensure they all pass
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes needed to the documentation

--- next-cloudinary/src/index.ts
+++ next-cloudinary/src/index.ts
@@ -18,4 +18,13 @@ export type { CloudinaryLoader, CloudinaryLoaderLoaderOptions, CloudinaryLoaderC
 
 export type { CloudinaryVideoPlayer, CloudinaryVideoPlayerOptions, CloudinaryVideoPlayerOptionsColors, CloudinaryVideoPlayerOptionsLogo } from './types/player';
 
-export * from './helpers';
+export { getCldImageUrl } from './helpers/getCldImageUrl';
+// @deprecated GetCldImageUrl
+export type { GetCldImageUrl, GetCldImageUrlOptions, GetCldImageUrlConfig, GetCldImageUrlAnalytics } from './helpers/getCldImageUrl';
+
+export { getCldOgImageUrl } from './helpers/getCldOgImageUrl';
+// @deprecated GetCldOgImageUrl
+export type { GetCldOgImageUrl, GetCldOgImageUrlOptions } from './helpers/getCldOgImageUrl';
+
+export { getCldVideoUrl } from './helpers/getCldVideoUrl';
+export type { GetCldVideoUrlOptions, GetCldVideoUrlConfig, GetCldVideoUrlAnalytics } from './helpers/getCldVideoUrl';
