cb53846e7bd452c61519cf4870e1508a81f6b7c3
Colby Fayock <fayock@gmail.com>
2024-01-17 11:43:23 -0500

    exporting CldUploadWidgetInfo type

--- next-cloudinary/src/index.ts
+++ next-cloudinary/src/index.ts
@@ -8,7 +8,7 @@ export { default as CldUploadButton } from './components/CldUploadButton';
 export type { CldUploadButtonProps } from './components/CldUploadButton';
 
 export { default as CldUploadWidget } from './components/CldUploadWidget';
-export type { CldUploadWidgetProps, CldUploadWidgetPropsChildren, CldUploadWidgetPropsOptions, CldUploadWidgetResults } from './components/CldUploadWidget';
+export type { CldUploadWidgetInfo, CldUploadWidgetProps, CldUploadWidgetPropsChildren, CldUploadWidgetPropsOptions, CldUploadWidgetResults } from './components/CldUploadWidget';
 
 export { default as CldVideoPlayer } from './components/CldVideoPlayer';
 export type { CldVideoPlayerProps, CldVideoPlayerPropsColors, CldVideoPlayerPropsLogo } from './components/CldVideoPlayer';
