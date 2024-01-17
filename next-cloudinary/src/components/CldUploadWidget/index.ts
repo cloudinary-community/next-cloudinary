8058c5ce8e7c399cde46eff7d3628a5495ce7d7d
Colby Fayock <colbyfayock@users.noreply.github.com>
2024-01-17 11:55:52 -0500

    feat: Export CldUploadWidgetInfo Type (#413)
    # Description
    
    Exports the `CldUploadWidgetInfo` type for consumption.
    
    ## Issue Ticket Number
    
    Fixes #412
    
    <!-- Specify above which issue this fixes by referencing the issue
    number (`#<ISSUE_NUMBER>`) or issue URL. -->
    <!-- Example: Fixes
    https://github.com/cloudinary-community/next-cloudinary/issues/<ISSUE_NUMBER>
    -->
    
    ## Type of change
    
    <!-- Please select all options that are applicable. -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Fix or improve the documentation
    - [ ] This change requires a documentation update
    
    
    # Checklist
    
    <!-- These must all be followed and checked. -->
    
    - [ ] I have followed the contributing guidelines of this project as
    mentioned in [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [ ] I have created an
    [issue](https://github.com/cloudinary-community/next-cloudinary/issues)
    ticket for this PR
    - [ ] I have checked to ensure there aren't other open [Pull
    Requests](https://github.com/cloudinary-community/next-cloudinary/pulls)
    for the same update/change?
    - [ ] I have performed a self-review of my own code
    - [ ] I have run tests locally to ensure they all pass
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes needed to the documentation

--- next-cloudinary/src/components/CldUploadWidget/index.ts
+++ next-cloudinary/src/components/CldUploadWidget/index.ts
@@ -1,2 +1,8 @@
 export { default } from './CldUploadWidget';
-export type { CldUploadWidgetProps, CldUploadWidgetPropsChildren, CldUploadWidgetPropsOptions, CldUploadWidgetResults } from './CldUploadWidget.types';
+export type {
+  CldUploadWidgetInfo,
+  CldUploadWidgetProps,
+  CldUploadWidgetPropsChildren,
+  CldUploadWidgetPropsOptions,
+  CldUploadWidgetResults,
+} from './CldUploadWidget.types';
