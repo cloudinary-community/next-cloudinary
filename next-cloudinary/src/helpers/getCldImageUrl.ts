e868afd19f81552d7aec3b7a90e88786c672c633
Colby Fayock <fayock@gmail.com>
2024-02-22 15:51:43 -0500

    feat: fixing analytics bug

--- next-cloudinary/src/helpers/getCldImageUrl.ts
+++ next-cloudinary/src/helpers/getCldImageUrl.ts
@@ -1,7 +1,7 @@
 import { constructCloudinaryUrl } from '@cloudinary-util/url-loader';
 import type { ImageOptions, ConfigOptions, AnalyticsOptions } from '@cloudinary-util/url-loader';
 
-import { NEXT_CLOUDINARY_ANALYTICS_ID, NEXT_CLOUDINARY_VERSION, NEXT_VERSION } from '../constants/analytics';
+import { NEXT_CLOUDINARY_ANALYTICS_PRODUCT_ID, NEXT_CLOUDINARY_ANALYTICS_ID, NEXT_CLOUDINARY_VERSION, NEXT_VERSION } from '../constants/analytics';
 import {checkForCloudName} from "../lib/cloudinary";
 
 /**
@@ -23,6 +23,7 @@ export function getCldImageUrl(options: GetCldImageUrlOptions, config?: GetCldIm
       },
     }, config),
     analytics: Object.assign({
+      product: NEXT_CLOUDINARY_ANALYTICS_PRODUCT_ID,
       sdkCode: NEXT_CLOUDINARY_ANALYTICS_ID,
       sdkSemver: NEXT_CLOUDINARY_VERSION,
       techVersion: NEXT_VERSION,
