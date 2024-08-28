f39178c381339ff23b850c1d761c174f6dc774ae
Colby Fayock <fayock@gmail.com>
2024-08-28 15:31:42 -0400

    adding Extract feature via update, adding docs

--- docs/components/HeaderImage/HeaderImage.js
+++ docs/components/HeaderImage/HeaderImage.js
@@ -1,11 +1,14 @@
 import styles from './HeaderImage.module.scss';
 
-export const HeaderImage = ({ children, layout }) => {
+export const HeaderImage = ({ children, layout, caption }) => {
   return (
     <div className={`${styles.headerImage} bg-slate-100 border border-slate-200 dark:bg-black dark:border-[#1c2428]`}>
       <div className={styles.headerImageChildren} data-layout={layout}>
         { children }
       </div>
+      {caption && (
+        <p className="mt-4 !-mb-3 text-sm text-center italics">{ caption }</p>
+      )}
     </div>
   );
 };
